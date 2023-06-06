3287fdab68f08caab5395c58f1824b700ce1444f
LAPTOP-60A1T1QF\yschen <yschen25@gmail.com>
2023-06-06 18:33:21 +0100

    fix bugs

--- src/components/Btn/index.js
+++ src/components/Btn/index.js
@@ -2,12 +2,7 @@ import './index.scss';
 import React, { useState, useEffect } from 'react';
 
 function Btn() {
- 
-
-  return (
-      <button></button>
-  );
+  return <button></button>;
 }
 
-
-export default function Btn();
+export default Btn;
